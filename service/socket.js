095740c661c1b95be768258343fd514c7f2a697b
陈宇汐文 <inpm.cy@gmail.com>
2021-05-12 16:39:11 +0800

    简化信令服务只处理join、leave逻辑

--- service/socket.js
+++ service/socket.js
@@ -15,47 +15,41 @@ const socketServer = (app) => {
   }
 
   io.sockets.on('connection', socket => {
-    let roomLen = 0
-    console.log(new Date().toString(), '「 A user is connected! 」')
+    socket.on('message', (room, data)=>{
+      socket.to(room).emit('message',room, data);
+    });
   
-    socket.on('create room', async room => {
-      ++roomLen
-      await socket.join(room)
-      await io.emit('wait join room', room)
-      outputLog('wait join room ' + room)
-    })
-
-    socket.on('join room', async room => {
-      ++roomLen
-      if (roomLen > 2) {
-        await io.emit('full')
-      } else {
-        await io.to(room).emit('joined room', room)
-        // await io.emit('joined room', room)
-        outputLog('join room', room)
+    socket.on('join', (room)=>{
+      socket.join(room);
+      var myRoom = io.sockets.adapter.rooms[room]; 
+      var users = (myRoom)? Object.keys(myRoom.sockets).length : 0;
+      logger.debug('the user number of room is: ' + users);
+  
+      if(users < USERCOUNT){
+        socket.emit('joined', room, socket.id); //发给除自己之外的房间内的所有人
+        if(users > 1){
+          socket.to(room).emit('otherjoin', room, socket.id);
+        }
+      
+      }else{
+        socket.leave(room);	
+        socket.emit('full', room, socket.id);
       }
-      console.log('roomLen', roomLen)
-    })
-
-    socket.on('local stream desc', async desc => {
-      await io.emit('got localPeerConnection desc', desc)
-      outputLog('local stream desc', desc)
-    })
-
-    socket.on('remote stream desc', async desc => {
-      await io.emit('got remotePeerConnection desc', desc)
-      outputLog('remote stream desc', desc)
-    })
-
-    socket.on('local stream candidate', async candidate => {
-      await io.emit('got remotePeerConnection candidate', candidate)
-      outputLog('local stream candidate', candidate)
-    })
-
-    socket.on('remote stream candidate', async candidate => {
-      await io.emit('got localPeerConnection candidate', candidate)
-      outputLog('remote stream candidate', candidate)
-    })
+      //socket.emit('joined', room, socket.id); //发给自己
+      //socket.broadcast.emit('joined', room, socket.id); //发给除自己之外的这个节点上的所有人
+      //io.in(room).emit('joined', room, socket.id); //发给房间内的所有人
+    });
+  
+    socket.on('leave', (room)=>{
+      var myRoom = io.sockets.adapter.rooms[room]; 
+      var users = (myRoom)? Object.keys(myRoom.sockets).length : 0;
+      logger.debug('the user number of room is: ' + (users-1));
+      //socket.emit('leaved', room, socket.id);
+      //socket.broadcast.emit('leaved', room, socket.id);
+      socket.to(room).emit('bye', room, socket.id);
+      socket.emit('leaved', room, socket.id);
+      //io.in(room).emit('leaved', room, socket.id);
+    });
   })
 
   io.sockets.on('disconnect', socket => {
