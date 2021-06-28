41707f83da0fb31b8a1a06c1071fe1746c72fd39
陈宇汐文 <inpm.cy@gmail.com>
2021-06-28 10:32:29 +0800

    fix

--- main.js
+++ main.js
@@ -4,20 +4,6 @@ const Koa = require('koa')
 const app = new Koa()
 // const routerServer = require('./service/static.js')
 const socketServer = require('./service/socket.js')
-app.use((req, res, next) => {
-  //判断路径
-    if(req.path !== '/' && !req.path.includes('.')){
-      res.set({
-        'Access-Control-Allow-Credentials': true, //允许后端发送cookie
-        'Access-Control-Allow-Origin': req.headers.origin || '*', //任意域名都可以访问,或者基于我请求头里面的域
-        'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type', //设置请求头格式和类型
-        'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',//允许支持的请求方式
-        'Content-Type': 'application/json; charset=utf-8'//默认与允许的文本格式json和编码格式
-      })
-    }
-    req.method === 'OPTIONS' ? res.status(204).end() : next()
-  })
-
 // // 错误捕获
 // app.on('error', async err => {
 //   console.error('\x1b[91m', `server error:${err}`)
